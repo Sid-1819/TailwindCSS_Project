74263db7f194abd4532e72e734abb64a096f01ca
Sid-1819 <siddhesh.shirdhankar18@gmail.com>
2024-08-02 18:01:57 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#54BD95',
+        'primaryLink': '#A6A6A6'
+      }
+    },
+  },
+  plugins: [],
+}
+
